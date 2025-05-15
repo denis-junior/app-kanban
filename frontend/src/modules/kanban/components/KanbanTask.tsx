e4c8ab04419c879bc0cf2f41ca0f0a6d26c4bc61
denis junior <denis_jr2001@hotmail.com>
2025-05-15 13:44:01 -0300

    feat: add columnId and handleEditTask props to KanbanTask component for enhanced task editing

--- frontend/src/modules/kanban/components/KanbanTask.tsx
+++ frontend/src/modules/kanban/components/KanbanTask.tsx
@@ -1,7 +1,17 @@
 import type { Task } from "../interfaces/task";
+import EditIcon from "@mui/icons-material/Edit";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
-export function KanbanTask({ task }: { task: Task }) {
-
+export function KanbanTask({
+  task,
+  columnId,
+  handleEditTask,
+}: {
+  task: Task;
+  columnId: string;
+  handleEditTask: (taskId: string, columnId: string) => void;
+}) {
   return (
     <div
       style={{
@@ -9,11 +19,29 @@ export function KanbanTask({ task }: { task: Task }) {
         backgroundColor: "white",
         borderRadius: "4px",
         boxShadow: "0 1px 3px rgba(0,0,0,0.1)",
-        cursor: "grab",
       }}
     >
       <h4>{task.title}</h4>
       {task.description && <p>{task.description}</p>}
+      <div className="d-flex justify-content-between">
+        <div
+          style={{
+            visibility: task.status === "pending" ? "hidden" : "visible",
+          }}
+        >
+          <KeyboardArrowLeftIcon />
+        </div>
+        <div onClick={() => handleEditTask(task.id, columnId)}>
+          <EditIcon />
+        </div>
+        <div
+          style={{
+            visibility: task.status === "done" ? "hidden" : "visible",
+          }}
+        >
+          <KeyboardArrowRightIcon />
+        </div>
+      </div>
     </div>
   );
 }
