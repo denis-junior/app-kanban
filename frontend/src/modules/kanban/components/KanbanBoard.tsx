2501d35a2ceea680ace5c306392ded8ecfc760ab
denis junior <denis_jr2001@hotmail.com>
2025-05-15 15:47:44 -0300

    feat: added kanban navbar

--- frontend/src/modules/kanban/components/KanbanBoard.tsx
+++ frontend/src/modules/kanban/components/KanbanBoard.tsx
@@ -1,21 +1,25 @@
-import { useKanban } from '../hooks/useKanban';
-import { KanbanColumn } from './KanbanColumn';
+import GeneralNavbar from "./KanbanNavbar";
+import { useKanban } from "../hooks/useKanban";
+import { KanbanColumn } from "./KanbanColumn";
 
 export function KanbanBoard() {
   const { columns } = useKanban();
 
   return (
-    <div className="kanban-board" style={{ 
-      display: 'flex',
-      gap: '1rem', 
-      padding: '1rem' 
-    }}>
-      {columns.map(column => (
-        <KanbanColumn 
-          key={column.id} 
-          column={column} 
-        />
-      ))}
-    </div>
+    <>
+      <GeneralNavbar />
+      <div
+        className="kanban-board"
+        style={{
+          display: "flex",
+          gap: "1rem",
+          padding: "1rem",
+        }}
+      >
+        {columns.map((column) => (
+          <KanbanColumn key={column.id} column={column} />
+        ))}
+      </div>
+    </>
   );
 }
